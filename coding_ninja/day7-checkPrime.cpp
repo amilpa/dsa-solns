4e9b090c3d917aaa55c275183f9c5dbec520f5c2
Amil P A <amilpa2020@gmail.com>
2023-10-11 23:07:54 +0530

    add

---
+++ coding_ninja/day7-checkPrime.cpp
@@ -0,0 +1,27 @@
+#include<cmath>
+bool isPrime(int n)
+{
+	// Write your code here.
+  if(n==1)
+  {
+    return false;
+  }
+  if(n==2)
+  {
+    return true;
+  }
+  if( n % 2 == 0 )
+  {
+    return false;
+  }
+  int i = 3;
+  int flag = 0;
+  while(i<=std::sqrt(n))
+  {
+    if( n % i == 0 ){
+      return false;
+    }
+    i++;
+  }
+  return true;
+}

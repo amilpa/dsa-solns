7244630cf0493d9d43b8d18f7a806d1c47d6e9f7
Amil P A <amilpa2020@gmail.com>
2023-10-11 22:54:29 +0530

    add

---
+++ coding_ninja/day6-sumOfAllDivisors.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+int sumOfAllDivisors(int n){
+	// Write your code here.	
+  int sum = 0;
+  int div = 1;
+  for(int i = 1;i<=n;i++)
+  {
+    div = 1;
+    while(div<=i/2)
+    {
+      if(i % div == 0)
+      {
+        sum += div; 
+      }
+      div++;
+    }
+    sum+=i;
+  }
+  return sum;
+}

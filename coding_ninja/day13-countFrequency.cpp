84881a3bdbb5e317cadc5bbdf1c8f3c06331541f
Amil P A <amilpa2020@gmail.com>
2023-10-14 23:03:42 +0530

    add

---
+++ coding_ninja/day13-countFrequency.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+
+std::vector<int> countFrequency(int n, int x, std::vector<int> &nums){
+    // Write your code here.
+    std::vector<int> freq(n,0);
+
+    int i;
+    for(i = 0;i<nums.size();i++)
+    {
+      freq[nums[i]-1]++;
+    }
+    return freq;
+}
